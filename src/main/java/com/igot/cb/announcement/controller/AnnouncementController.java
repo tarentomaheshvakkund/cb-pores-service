1fae9c626a08479ccdc24b79f89b5d2669b0273d
ruksana2808 <111083125+ruksana2808@users.noreply.github.com>
2024-05-31 12:15:07 +0530

    changed the base url for announcement controller as this base url is already defined for announcement_service (#37)

--- src/main/java/com/igot/cb/announcement/controller/AnnouncementController.java
+++ src/main/java/com/igot/cb/announcement/controller/AnnouncementController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/announcement")
+@RequestMapping("/announcements")
 public class AnnouncementController {
 
   @Autowired
