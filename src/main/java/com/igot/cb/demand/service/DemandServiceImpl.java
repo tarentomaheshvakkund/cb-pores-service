ed690a329d3e88047a581ab1909c0055eb44193c
ruksanasemeir <ruksana.semeir@tarento.com>
2024-06-10 18:44:36 +0530

    Made changes for review comments

--- src/main/java/com/igot/cb/demand/service/DemandServiceImpl.java
+++ src/main/java/com/igot/cb/demand/service/DemandServiceImpl.java
@@ -122,7 +122,7 @@ public class DemandServiceImpl implements DemandService {
                 ((ObjectNode) demandDetails).put(Constants.CREATED_ON, String.valueOf(currentTime));
                 jsonNodeEntity.setCreatedOn(currentTime);
             }
-            if (demandDetails.has(Constants.DEMAND_ID) && demandDetails.get(Constants.REQUEST_TYPE).asText().equalsIgnoreCase(Constants.SINGLE)){
+            if (demandDetails.has(Constants.DEMAND_ID) && Constants.SINGLE.equalsIgnoreCase(demandDetails.get(Constants.REQUEST_TYPE).asText())){
                 Optional<DemandEntity> demandEntity = demandRepository.findById(
                     demandDetails.get(Constants.DEMAND_ID).asText());
                 if (demandEntity.isPresent()) {
